8f4b98f82266b150caf4f7688230a4518f7f110b
Aaryan Mishra <aaryan.mshra@gmail.com>
2024-06-01 14:31:14 +0530

    .

--- server.js
+++ server.js
@@ -8,10 +8,7 @@ import cors from 'cors';
 dotenv.config()
 
 const app = express();
-app.use(cors({
-  origin: `${process.env.SITE}`,
-  optionsSuccessStatus: 200
-}))
+app.use(cors())
 app.use(express.json())
 
 mongoose.connect(process.env.DB_URI)
